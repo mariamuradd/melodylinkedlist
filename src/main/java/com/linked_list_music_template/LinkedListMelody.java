e0b9ad8cc423096997d85e1206bb3eccaa5fb0dc
mariamuradd <mmmurad@smu.edu>
2024-10-17 16:49:01 -0500

    Initial commit

---
+++ src/main/java/com/linked_list_music_template/LinkedListMelody.java
@@ -0,0 +1,18 @@
+/*
+ * c2024  [your name] using a template by Dr. Courtney Brown
+ * Class: LinkedListMelody
+ * Description: [you fille in]
+ * 
+ */
+
+
+package com.linked_list_music_template;
+
+public class LinkedListMelody implements Drawable {
+
+    //fill in this class
+    public void draw()
+    {
+        //fill this in to play melody
+    }
+}
---
+++ src/main/java/com/linked_list_music_template/OnMousePress.java
@@ -0,0 +1,16 @@
+package com.linked_list_music_template;
+
+
+/*
+ * c2024 Oct Courtney Brown 
+ * 
+ * Interface: OnMousePress
+ * Description: Anything that reacts to a Processing mousepress
+ * 
+ */
+
+
+public interface OnMousePress {
+    abstract void onPress();
+    abstract void mousePressed(float mx, float my);
+}
